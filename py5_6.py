d541ab773457a408ed3798f4b1e2dddf5ada457b
Jongwan Kim <84608455+liaslab@users.noreply.github.com>
2021-05-22 14:13:59 +0900

    Update py5_6.py

--- py5_6.py
+++ py5_6.py
@@ -1,6 +1,8 @@
 i, dan = 0, 0
 
+print("구구단 출력 프로그램")
 for dan in range(2, 10, 1):
     for i in range(1, 10, 1):
         print("%d X %d = %2d" % (dan, i, dan*i))
     print("----------")
+print("끝")
